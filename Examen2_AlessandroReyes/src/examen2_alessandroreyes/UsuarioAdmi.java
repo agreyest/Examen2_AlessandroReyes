4546bb5c9662718f8c4f6b502e74bb5084e0b80b
agreyest <56709605+agreyest@users.noreply.github.com>
2019-12-13 16:15:01 -0600

    binario de usuario
    terminado

--- Examen2_AlessandroReyes/src/examen2_alessandroreyes/UsuarioAdmi.java
+++ Examen2_AlessandroReyes/src/examen2_alessandroreyes/UsuarioAdmi.java
@@ -1,5 +1,82 @@
 package examen2_alessandroreyes;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class UsuarioAdmi {
+    private ArrayList<Usuario> listasusuarios = new ArrayList();
+    private File archivo = null;
     
+    public UsuarioAdmi(String path) {
+        archivo = new File(path);
+    }
+
+    public ArrayList<Usuario> getListaMsdm() {
+        return listasusuarios;
+    }
+
+    public void setListaMsdm(ArrayList<Usuario> listamsdm) {
+        this.listasusuarios = listamsdm;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void setMdm(Usuario a){
+        listasusuarios.add(a);
+    }
+    
+    public void cargarArchivo() {
+        try {            
+            listasusuarios = new ArrayList();
+            Usuario temp;
+            if (archivo.exists()) {
+                FileInputStream entrada
+                    = new FileInputStream(archivo);
+                ObjectInputStream objeto
+                    = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Usuario) objeto.readObject()) != null) {
+                        listasusuarios.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            }            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(archivo);
+            bw = new ObjectOutputStream(fw);
+            for (Usuario t : listasusuarios) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
 }
